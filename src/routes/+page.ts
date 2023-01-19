f632d9f3312bd24bb655889ecff574207f49f61c
jeffpohlmeyer <jeffvp@pm.me>
2023-01-19 01:12:19 -0500

    feat: adding redirect

---
+++ src/routes/+page.ts
@@ -0,0 +1,8 @@
+import { redirect } from '@sveltejs/kit';
+import type { PageLoad } from './$types';
+
+export const load: PageLoad = async () => {
+	const a = Math.floor(Math.random() * 11);
+	const b = Math.floor(Math.random() * 11);
+	throw redirect(301, `/multiply/${a}/${b}`);
+};

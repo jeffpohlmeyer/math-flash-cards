5d6dddf7c9f0205bac6e23aa05733acb93764225
jeffreypohlmeyer <jeffvp@protonmail.ch>
2024-07-03 06:49:53 -0400

    updating svelte and sveltekit

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -4,5 +4,5 @@ import type { PageLoad } from './$types';
 export const load: PageLoad = async () => {
 	const a = Math.floor(Math.random() * 11);
 	const b = Math.floor(Math.random() * 11);
-	throw redirect(301, `/multiply/${a}/${b}`);
+	redirect(301, `/multiply/${a}/${b}`);
 };
